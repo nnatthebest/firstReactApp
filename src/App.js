2f84b1e39cf099a89daba46079847cc26dc59f7e
Daniil Biyakov <biyakov.den@gmail.com>
2023-08-18 21:22:33 +0600

    Add grid style

--- src/App.js
+++ src/App.js
@@ -23,24 +23,17 @@ function Board({xIsNext, squares, onPlay, currentMove}) {
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
+  const squaresMap = squares.map((element, index) => {
+    return (
+        <Square key={index} value={element} onSquareClick={() => handleClick(index)}/>
+    )
+  })
   return (
     <div>
       <div className='status'>{status}</div>
       <div className='current-move'>Current move: {currentMove + 1}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      <div className="board-grid">
+        {squaresMap}
       </div>
     </div>
   );
